d8c08ad167cc062d4484001579817296708d6f35
DenUP <ctrike163@gmail.com>
2024-06-20 09:41:18 +0300

    ui(stf) + STL + provider

--- lib/example/example_widget.dart
+++ lib/example/example_widget.dart
@@ -12,7 +12,7 @@ class ExampleWidget extends StatefulWidget {
 
 class _ExampleWidgetState extends State<ExampleWidget> {
   var _posts = <dynamic>[];
-  Future<void> _onLoading() async {
+  Future<void> onLoading() async {
     final url = Uri.parse('https://jsonplaceholder.typicode.com/posts');
     final response = await get(url);
     final json = jsonDecode(response.body) as List<dynamic>;
@@ -22,26 +22,63 @@ class _ExampleWidgetState extends State<ExampleWidget> {
 
   @override
   Widget build(BuildContext context) {
+    return Provider(state: this, child: const _View());
+  }
+}
+
+class _View extends StatelessWidget {
+  const _View({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final state = context.dependOnInheritedWidgetOfExactType<Provider>()!.state;
     return Scaffold(
       body: SafeArea(
           child: Column(
         children: [
           ElevatedButton(
-              onPressed: _onLoading, child: const Text('Loading post')),
+              onPressed: state.onLoading, child: const Text('Loading post')),
           Expanded(
               child: ListView.builder(
-            itemCount: _posts.length,
+            itemCount: state._posts.length,
             itemBuilder: (context, index) {
-              return Column(
-                children: [
-                  Text(_posts[index]['title']),
-                  Text(_posts[index]['id'].toString())
-                ],
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Text(state._posts[index]['title']),
+                    Text(state._posts[index]['id'].toString())
+                  ],
+                ),
               );
             },
           ))
         ],
       )),
     );
+  }
+}
+
+class Provider extends InheritedWidget {
+  final _ExampleWidgetState state;
+  const Provider({
+    required this.state,
+    required super.child,
+  });
+
+  static Provider? watch(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<Provider>();
+  }
+
+  static Provider? read(BuildContext context) {
+    final widget =
+        context.getElementForInheritedWidgetOfExactType<Provider>()?.widget;
+    return widget is Provider ? widget : null;
+  }
+
+  @override
+  bool updateShouldNotify(InheritedWidget oldWidget) {
+    // TODO: implement updateShouldNotify
+    return child != oldWidget.child;
   }
 }
