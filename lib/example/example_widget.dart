b39f58c2b1aacfc25b5c8b2cc1cd79c31c712342
DenUP <ctrike163@gmail.com>
2024-06-22 18:52:07 +0300

    example 2

--- lib/example/example_widget.dart
+++ lib/example/example_widget.dart
@@ -1,7 +1,38 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+class ViewModel extends ChangeNotifier {
+  var _age = 0;
+
+  int get age => _age;
+
+  ViewModel() {
+    _loadData();
+  }
+
+  Future<void> _loadData() async {
+    final sharedPreferences = await SharedPreferences.getInstance();
+    _age = sharedPreferences.getInt('age') ?? 0;
+    notifyListeners();
+  }
+
+  Future<void> increment() async {
+    _age += 1;
+    final sharedPreferences = await SharedPreferences.getInstance();
+    await sharedPreferences.setInt('age', _age);
+    notifyListeners();
+  }
+
+  Future<void> decrement() async {
+    _age = max(_age - 1, 0);
+    final sharedPreferences = await SharedPreferences.getInstance();
+    await sharedPreferences.setInt('age', _age);
+    notifyListeners();
+  }
+}
 
 class ExampleWidget extends StatefulWidget {
   const ExampleWidget({super.key});
@@ -11,45 +42,25 @@ class ExampleWidget extends StatefulWidget {
 }
 
 class _ExampleWidgetState extends State<ExampleWidget> {
-  var _value = 0;
-  Future<void> _loadData() async {
-    final sharedPreferences = await SharedPreferences.getInstance();
-    _value = sharedPreferences.getInt('age') ?? 0;
-    setState(() {});
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _loadData();
-  }
-
   @override
   Widget build(BuildContext context) {
+    final model = context.watch<ViewModel>();
     return Scaffold(
       body: SafeArea(
         child: Center(
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              Text("$_value"),
+              Text("${model._age}"),
               ElevatedButton(
                 onPressed: () async {
-                  _value += 1;
-                  final sharedPreferences =
-                      await SharedPreferences.getInstance();
-                  await sharedPreferences.setInt('age', _value);
-                  setState(() {});
+                  model.increment();
                 },
                 child: const Text('+'),
               ),
               ElevatedButton(
                 onPressed: () async {
-                  _value = max(_value -= 1, 0);
-                  final sharedPreferences =
-                      await SharedPreferences.getInstance();
-                  await sharedPreferences.setInt('age', _value);
-                  setState(() {});
+                  model.decrement();
                 },
                 child: const Text('-'),
               ),
--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,5 +1,6 @@
 import 'package:architecture_studying/example/example_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -10,7 +11,8 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       debugShowCheckedModeBanner: false,
       routes: {
-        '/': (context) => const ExampleWidget(),
+        '/': (context) => ChangeNotifierProvider.value(
+            value: ViewModel(), child: const ExampleWidget()),
       },
     );
   }
