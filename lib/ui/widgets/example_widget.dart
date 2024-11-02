6e9ea2a5042b8b02ff70f226c0a605655d5790dd
DenUP <ctrike163@gmail.com>
2024-11-02 14:22:13 +0300

    add: loader widget

---
+++ lib/ui/widgets/example_widget.dart
@@ -0,0 +1,38 @@
+import 'package:architecture_studying/service/auth_service.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class _ViewModel extends ChangeNotifier {
+  final _authService = AuthService();
+
+  Future<void> onClickButtonLogOut(context) async {
+    await _authService.logOut();
+    Navigator.of(context).pushNamedAndRemoveUntil('/', (router) => false);
+  }
+}
+
+class ExampleWidget extends StatelessWidget {
+  const ExampleWidget({super.key});
+
+  static Widget create() {
+    return ChangeNotifierProvider(
+      create: (_) => _ViewModel(),
+      child: const ExampleWidget(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final model = context.read<_ViewModel>();
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          ElevatedButton(
+              onPressed: () => model.onClickButtonLogOut(context),
+              child: Text('Выход'))
+        ],
+      ),
+      body: SafeArea(child: Text('Главная страница')),
+    );
+  }
+}
