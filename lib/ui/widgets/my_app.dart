161330d37d018eb10c118e413264edce98da5b9c
DenUP <ctrike163@gmail.com>
2024-11-14 17:44:19 +0300

    add: User bloc

--- lib/ui/widgets/my_app.dart
+++ lib/ui/widgets/my_app.dart
@@ -1,6 +1,7 @@
+import 'package:architecture_studying/domain/bloc/userbloc.dart';
 import 'package:architecture_studying/ui/widgets/example_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -11,7 +12,10 @@ class MyApp extends StatelessWidget {
       // initialRoute: '/',
       debugShowCheckedModeBanner: false,
       routes: {
-        '/': (context) => ExampleWidget(),
+        '/': (context) => Provider(
+              child: const ExampleWidget(),
+              create: (_) => Userbloc(),
+            ),
       },
     );
   }
